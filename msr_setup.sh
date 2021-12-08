e12dda4b54123a4dfc8061aae0f6d9cdfd9c6752
dah02 <92973975+dah02@users.noreply.github.com>
2021-12-08 17:20:38 +0900

    Update msr_setup.sh

--- msr_setup.sh
+++ msr_setup.sh
@@ -9,6 +9,7 @@ pip3 install wheel
 pip3 install pandas
 sudo apt-get install portaudio19-dev
 pip3 install pyaudio
+sudo apt-get install python3-sdl2
 pip3 install pygame
 pip3 install tweepy==3.10.0
 pip3 install konlpy
