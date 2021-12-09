a4a0522ff309a35f3ccda0b896986e755ad2f88c
dah02 <92973975+dah02@users.noreply.github.com>
2021-12-09 17:23:59 +0900

    kakao api 추가

--- connect.py
+++ connect.py
@@ -2,16 +2,32 @@ from ibm_watson import AssistantV2
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson.text_to_speech_v1 import TextToSpeechV1
 from pygame import mixer
+import os
 import time
+import requests
 
 # Audio recording parameters
 RATE = 16000
 CHUNK = int(RATE / 10)  # 100ms
 
+HIGH = 1
+LOW = 0
+
+
+def isNumber(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
 
 class Connection:
     def __init__(self):
         # speech to text variants
+        self.kakao_account = 'f8f8c3f66bb3310016fdeccffba152e8'
+        os.system(f'gpio mode 7 out;gpio write 7 {HIGH}')
+
         self.STREAMING_LIMIT = 240000  # 4 minutes
         self.SAMPLE_RATE = 16000
         self.CHUNK_SIZE = int(self.SAMPLE_RATE / 10)  # 100ms
@@ -80,8 +96,46 @@ class Connection:
         bitsize = -16
         channels = 1
         buffer = 2048
-        mixer.init(freq, bitsize, channels, buffer)
-        mixer.Sound(audio_file_name).play()
+        mixer.init()
+        # mixer.init(freq, bitsize, channels, buffer)
+        q = mixer.Sound(audio_file_name)
+        q.play()
         while mixer.get_busy() == True:
             time.sleep(1.0)
         # print(len(input_text))
+
+    # kakao tts api
+    def tts(self, string, filename="tts.wav"):
+
+        if self.kakao_account in [None, '']:
+            raise Exception('Kakao account invalid')
+
+        url = "https://kakaoi-newtone-openapi.kakao.com/v1/synthesize"
+        headers = {
+            'Content-Type': 'application/xml',
+            'Authorization': 'KakaoAK ' + self.kakao_account
+        }
+        r = requests.post(url, headers=headers, data=string.encode('utf-8'))
+        with open(filename, 'wb') as f:
+            f.write(r.content)
+
+    # kakao tts play
+    def play(self, filename, out='local', volume='-2000.0', background=True):
+
+        if not os.path.isfile(filename):
+            raise Exception(f'"{filename}" does not exist')
+
+        if not filename.split('.')[-1] in ['mp3', 'wav']:
+            raise Exception(f'"{filename}" must be (mp3|wav)')
+
+        if not out in ['local', 'hdmi', 'both']:
+            raise Exception(f'"{out}" must be (local|hdmi|both)')
+
+        if not isNumber(volume):
+            raise Exception(f'"{volume}" is not Number')
+
+        if type(background) != bool:
+            raise Exception(f'"{background}" is not bool')
+
+        opt = '&' if background else ''
+        os.system(f'omxplayer -o {out} --vol {volume} {filename} {opt}')
