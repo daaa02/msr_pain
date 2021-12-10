ce9a0de4d6f0a7aee0100715a5289fa98a02f07d
dah02 <92973975+dah02@users.noreply.github.com>
2021-12-10 16:33:28 +0900

    Update main_inter.py

--- main_inter.py
+++ main_inter.py
@@ -1,9 +1,6 @@
-import os.path
-
 from before_func_inter import before_func
 
 if __name__ == '__main__':
-    # 이전 데이터가 없으면 조건 추가
     uid = input("uid 를 입력하세요.\n")
     folder = "Data/"
     file = folder + uid + ".txt"
