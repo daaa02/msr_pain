eff112e0866fab14d4b4fe5acf2f8723ba302aec
dah02 <92973975+dah02@users.noreply.github.com>
2021-12-10 16:34:07 +0900

    Update main_inter.py

--- main_inter.py
+++ main_inter.py
@@ -3,6 +3,6 @@ from before_func_inter import before_func
 if __name__ == '__main__':
     uid = input("uid 를 입력하세요.\n")
     folder = "Data/"
-    file = folder + uid + ".txt"
+    file = folder + uid + "_inter.txt"
     
     before_func(uid)
