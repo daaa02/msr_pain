4da8513f50bb3088e41dcac2d0c14e221c7a6b1e
dah02 <92973975+dah02@users.noreply.github.com>
2022-02-15 20:40:56 +0900

    Create stt_setup.sh
    google stt api 설치 방법

---
+++ stt_setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "install with pip version==19.2.3"
+echo "install with pip version==19.2.3"
+echo "install with pip version==19.2.3"
+echo "install with pip version==19.2.3"
+echo "install with pip version==19.2.3"
+
+echo "***INSTALL VENV***"
+sudo apt-get install python3-dev python3-venv
+python3 -m venv venv
+rm -rf ~/.cache/pip
+venv/bin/python -m pip install --upgrade pip setuptools wheel
+source venv/bin/activate
+
+echo "***INSTALL GOOGLE SPEECH-TO-TEXT***"
+sudo apt-get install portaudio19-dev libffi-dev libssl-dev
+pip3 install pyaudio
+pip3 install --upgrade google-cloud-storage
+pip3 install --upgrade google-cloud-speech
+
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-369.0.0-linux-x86.tar.gz
+tar -zxvf google-cloud-sdk-369.0.0-linux-x86.tar.gz
+gcloud init
+
+echo "***NEED TO START NEW SHELL***"
+
+exit 0
