b4ea2f9acca3dc0e90b8a1d1106f26b1315db4a3
dah02 <92973975+dah02@users.noreply.github.com>
2022-02-16 14:43:51 +0900

    Update msr_setup.sh

--- msr_setup.sh
+++ msr_setup.sh
@@ -1,25 +1,52 @@
 #!/bin/bash
 
-echo "***update***"
+echo "***UPDATE***"
 sudo apt-get update -y
 sudo apt-get upgrade -y
-pip install --upgrade pip
 
-echo "***install python library***"
+echo "***INSTALL VENV***"
+cd
+sudo apt-get install python3-dev python3-venv
+python3 -m venv venv
+rm -rf ~/.cache/pip
+venv/bin/python -m pip install --upgrade pip setuptools wheel
+source venv/bin/activate
+
+echo "***INSTALL PYTHON LIBRARY***"
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
 pip3 install wheel
-pip3 install grpcio==1.40.0
-pip3 install numpy==1.21.0
 pip3 install pandas
-sudo apt-get install portaudio19-dev
-pip3 install pyaudio
-sudo apt-get install python3-sdl2
-pip3 install pygame
+pip3 install grpcio==1.40.0 -y
+pip3 install numpy==1.21.0
 pip3 install tweepy==3.10.0
+pip install grpcio==1.40.0
+sudo pip3 install -U grpcio --no-binary=grpcio
+sudo pip3 install -U grpcio-tools --no-binary=grpcio-tools
+
+sudo apt-get install default-jdk
 pip3 install konlpy
-pip3 install google-cloud-speech
+
+sudo apt-get install python3-sdl2
+sudo apt-get install portaudio19-dev libffi-dev libssl-dev
+pip3 install pyaudio
+pip3 install pygame
+sudo apt-get install omxplayer
+
+echo "***INSTALL IBM WATSON ASSISTANT***"
 pip3 install ibm-watson
 
-echo "***reboot***"
+echo "***INSTALL GOOGLE SPEECH-TO-TEXT***"
+sudo pip3 install --upgrade google-api-python-client
+pip3 install --upgrade google-cloud-storage
+pip3 install --upgrade google-cloud-speech
+
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-369.0.0-linux-x86.tar.gz
+tar -zxvf google-cloud-sdk-369.0.0-linux-x86.tar.gz
+gcloud init
+
+echo "***REBOOT & NEED TO START NEW SHELL***"
 sudo reboot
 
 exit 0
