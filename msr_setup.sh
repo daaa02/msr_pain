98fa4153d3095ed6c9d4d5d595ef8eb85cd85dfb
dah02 <92973975+dah02@users.noreply.github.com>
2022-02-16 13:43:06 +0900

    Update msr_setup.sh

--- msr_setup.sh
+++ msr_setup.sh
@@ -16,7 +16,7 @@ pip3 install tweepy==3.10.0
 pip3 install konlpy
 pip3 install tweepy
 pip3 install google-cloud-speech
-#pip3 install ibm-watson
+pip3 install ibm-watson
 
 echo "***reboot***"
 sudo reboot
